32e43fb0571cc7b6b7dd36a041a38235958224d7
Михаил Марченко <mehanikm@MacBook-ProM.local>
2019-04-05 00:51:35 +0200

    Add users to base

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+import json
 from time import sleep
 from config import *
 
@@ -9,10 +10,26 @@ bot = telebot.TeleBot(token)
 # Handle [start] command
 @bot.message_handler(commands=['start'])
 def start(message):
+    chat = message.chat.id
+    # Greet user
+    sleep(0.1)
+    bot.send_chat_action(chat, 'typing')
     sleep(0.5)
-    bot.send_chat_action(message.chat.id, 'typing')
-    sleep(1)
-    bot.send_message(message.chat.id, 'Text after start command')
+    bot.send_message(chat, f'Hey, {message.from_user.first_name}!')
+    sleep(0.1)
+    bot.send_chat_action(chat, 'typing')
+    sleep(0.5)
+
+    # If really new user, add him to base
+    if str(chat) not in users:
+        users[str(chat)] = dict()
+        users[str(chat)]["status"] = 0
+        users[str(chat)]["tasks"] = []
+        bot.send_message(
+            chat, "I've added you to my users database!")
+    else:
+        bot.send_message(
+            chat, 'You are already in my base of users!')
 
 
 # Handle [help] command
@@ -27,6 +44,13 @@ def help(message):
 # Handle [new] command
 @bot.message_handler(commands=['new'])
 def new_task(message):
+    newtask = dict()
+
+
+# Add task text
+@bot.message_handler(func=lambda message:
+                     users_status.get(message.chat.id) == TEXT)
+def add_text(message):
     pass
 
 
@@ -42,6 +66,15 @@ def del_task(message):
     pass
 
 
-# Start bot's polling
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    # Open users base
+    with open("dump.json") as f:
+        users = json.load(f)
+
+    # Start bot's polling
+    try:
+        bot.polling(none_stop=True)
+    # Save users base on stop
+    finally:
+        with open("dump.json", "w+") as f:
+            json.dump(users, f)
